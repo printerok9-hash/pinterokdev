5120a1a0638319c9308cb2b9016d994db57fa629
printerok9-hash <printerok9@gmail.com>
2026-09-21 00:12:29 +0530

    Added GTM-55Q44PST

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,13 @@ export default function RootLayout({
       className={roboto.variable}
     >
       <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-55Q44PST');`}
+        </Script>
         <Script
           id="google-tag"
           src="https://www.googletagmanager.com/gtag/js?id=G-QQ01JPEPGL"
@@ -81,7 +88,18 @@ ${GOOGLE_ADS_ID ? `gtag('config', ${JSON.stringify(GOOGLE_ADS_ID).replace(/</g, 
 };`}
         </Script>
       </head>
-      <body>{children}</body>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-55Q44PST"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
